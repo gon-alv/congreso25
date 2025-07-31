6fa5e8b4a90fc6047c94c534a11574465c5390aa
gon-alv <gonzalo.a.alv@gmail.com>
2025-07-31 15:12:23 -0400

    defined netlify build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    vite:{
-        plugins: [tailwindcss()],
-    },
-    redirects: {
-        '/hoteles/1': '/hoteles'
-    }
+  vite:{
+      plugins: [tailwindcss()],
+  },
+
+  redirects: {
+      '/hoteles/1': '/hoteles'
+  },
+
+  adapter: netlify()
 });
